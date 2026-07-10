c04c72fe226d852d55132b48f6d1e16762b1d00d
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-10 17:11:02 -0500

    refactor: remove legacy not found handler

--- src/middleware/not-found.js
+++
@@ -1,6 +0,0 @@
-export function notFoundHandler(request, response) {
-  response.status(404).render('errors/404', {
-    pageTitle: 'Página no encontrada',
-    requestedPath: request.originalUrl,
-  });
-}
