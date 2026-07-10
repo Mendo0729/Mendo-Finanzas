9dd8ff395ce213fa7461171d6e4f22267dfac304
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-10 17:39:45 -0500

    feat: register session and authentication middleware

--- src/bootstrap/register-middlewares.js
+++ src/bootstrap/register-middlewares.js
@@ -2,10 +2,20 @@ import compression from 'compression';
 import express from 'express';
 import helmet from 'helmet';
 
+import { createSessionMiddleware } from '../config/session.js';
 import { requestContext } from '../core/middleware/request-context.js';
+import { exposeAuthenticatedCsrfToken } from '../core/security/csrf.js';
+import { loadCurrentUser } from '../modules/auth/auth.middleware.js';
+
+function initializeViewLocals(_request, response, next) {
+  response.locals.currentUser = null;
+  response.locals.csrfToken = null;
+  next();
+}
 
 export function registerMiddlewares(app, { publicDirectory }) {
   app.use(requestContext);
+  app.use(initializeViewLocals);
   app.use(
     helmet({
       contentSecurityPolicy: {
@@ -21,7 +31,10 @@ export function registerMiddlewares(app, { publicDirectory }) {
     }),
   );
   app.use(compression());
+  app.use(express.static(publicDirectory));
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json({ limit: '100kb' }));
-  app.use(express.static(publicDirectory));
+  app.use(createSessionMiddleware());
+  app.use(loadCurrentUser);
+  app.use(exposeAuthenticatedCsrfToken);
 }
