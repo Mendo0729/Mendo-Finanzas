bebc80f805651718a304dbbb794d20e218166542
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-11 00:08:02 -0500

    feat: register household routes

--- src/bootstrap/register-routes.js
+++ src/bootstrap/register-routes.js
@@ -1,9 +1,11 @@
 import { authRouter } from '../modules/auth/auth.routes.js';
 import { healthRouter } from '../modules/health/health.routes.js';
 import { homeRouter } from '../modules/home/home.routes.js';
+import { householdRouter } from '../modules/households/household.routes.js';
 
 export function registerRoutes(app) {
   app.use('/', homeRouter);
   app.use('/auth', authRouter);
+  app.use('/households', householdRouter);
   app.use('/health', healthRouter);
 }
