1b355c38836a3fdc872fc763957a770a787b2962
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-11 02:00:11 -0500

    feat: register account and category routes

--- src/bootstrap/register-routes.js
+++ src/bootstrap/register-routes.js
@@ -1,4 +1,6 @@
+import { accountRouter } from '../modules/accounts/account.routes.js';
 import { authRouter } from '../modules/auth/auth.routes.js';
+import { categoryRouter } from '../modules/categories/category.routes.js';
 import { healthRouter } from '../modules/health/health.routes.js';
 import { homeRouter } from '../modules/home/home.routes.js';
 import { householdRouter } from '../modules/households/household.routes.js';
@@ -7,5 +9,7 @@ export function registerRoutes(app) {
   app.use('/', homeRouter);
   app.use('/auth', authRouter);
   app.use('/households', householdRouter);
+  app.use('/accounts', accountRouter);
+  app.use('/categories', categoryRouter);
   app.use('/health', healthRouter);
 }
