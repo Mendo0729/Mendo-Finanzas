2ee8df0062c94e39e80faf1af0f6f19a6898f41f
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-12 19:37:31 -0500

    feat: expose household creation and editing routes

--- src/modules/households/household.routes.js
+++ src/modules/households/household.routes.js
@@ -5,28 +5,69 @@ import { verifyCsrfToken } from '../../core/security/csrf.js';
 import { asyncHandler } from '../../core/utils/async-handler.js';
 import { requireAuthentication } from '../auth/auth.middleware.js';
 import {
+  createHousehold,
   selectHousehold,
+  showCreateHousehold,
   showCurrentHousehold,
+  showEditHousehold,
   showHouseholdSelection,
+  updateHousehold,
 } from './household.controller.js';
-import { requireHouseholdMembership } from './household.middleware.js';
-import { householdSelectionSchema } from './household.schemas.js';
+import {
+  requireHouseholdMembership,
+  requireHouseholdPermission,
+  requireHouseholdScope,
+} from './household.middleware.js';
+import { HOUSEHOLD_PERMISSIONS } from './household.roles.js';
+import {
+  householdIdParamsSchema,
+  householdSelectionSchema,
+  householdWriteSchema,
+} from './household.schemas.js';
 
 export const householdRouter = Router();
 
-householdRouter.get('/select', requireAuthentication, asyncHandler(showHouseholdSelection));
+householdRouter.use(requireAuthentication);
+
+householdRouter.get('/select', asyncHandler(showHouseholdSelection));
 
 householdRouter.post(
   '/select',
-  requireAuthentication,
   verifyCsrfToken,
   validate({ body: householdSelectionSchema }),
   asyncHandler(selectHousehold),
 );
 
+householdRouter.get('/new', showCreateHousehold);
+
+householdRouter.post(
+  '/',
+  verifyCsrfToken,
+  validate({ body: householdWriteSchema }),
+  asyncHandler(createHousehold),
+);
+
 householdRouter.get(
   '/current',
-  requireAuthentication,
   requireHouseholdMembership,
   showCurrentHousehold,
+);
+
+householdRouter.get(
+  '/:householdId/edit',
+  requireHouseholdMembership,
+  requireHouseholdPermission(HOUSEHOLD_PERMISSIONS.HOUSEHOLD_UPDATE),
+  validate({ params: householdIdParamsSchema }),
+  requireHouseholdScope((request) => request.validated.params.householdId),
+  asyncHandler(showEditHousehold),
+);
+
+householdRouter.post(
+  '/:householdId',
+  requireHouseholdMembership,
+  requireHouseholdPermission(HOUSEHOLD_PERMISSIONS.HOUSEHOLD_UPDATE),
+  verifyCsrfToken,
+  validate({ params: householdIdParamsSchema, body: householdWriteSchema }),
+  requireHouseholdScope((request) => request.validated.params.householdId),
+  asyncHandler(updateHousehold),
 );
